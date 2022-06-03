d792c8fac579d0277b19a853e26c05d26e29953f
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-06-03 16:56:37 -0300

    Update Corrimientos.java

--- Corrimientos.java
+++ Corrimientos.java
@@ -1,1 +1,18 @@
 
+// Corrimiento hacia izquierda. Sirve para eliminar una posicion
+	public static void corrimiento_izq(int[] arr,int pos) {
+		while (pos<MAX-1){
+			arr[pos] = arr[pos+1];
+			pos++;
+		}
+	}  
+
+
+// Corrimiento hacia derecha. Sirve para insertar un elemento
+	public static void corrimiento_der(int[] arr,int pos) {
+		int actual = MAX-1;
+		while (pos<actual){
+			arr[actual] = arr[actual-1];
+			actual--;
+		}
+	} 
